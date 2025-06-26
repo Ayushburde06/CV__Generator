08eebe8b9886dc0b44627896a4ebc330b2bb96a5
Ayushburde06 <ayushburde324@gmail.com>
2025-06-26 18:11:24 +0530

    A Django-based web application that allows users to generate and download professional CVs in PDF format from form inputs.
    Developed a Django web app that dynamically collects user data and generates professional CVs using HTML forms and PDFKit.
    
    Implemented form validation, PDF conversion, and templated rendering to automate resume creation and export.

--- pdf/views.py
+++ pdf/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Profile
+
 
 # Create your views here.
+def accept(request):
+    if request.method == "POST":
+        name= request.POST.get("name","")
+        email= request.POST.get("email","")
+        phone= request.POST.get("phone","")
+        summary= request.POST.get("summary","")
+        degree= request.POST.get("degree","")
+        school= request.POST.get("school","")
+        university= request.POST.get("university","")
+        project= request.POST.get("project","")
+        skills= request.POST.get("skills","")
+
+        profile = Profile(name=name,email=email,phone=phone,summary=summary,degree=degree,school=school,university=university,project=project,skills=skills)
+        profile.save()
+    return render(request,'pdf/accept.html')
